751217f52ba35e813c9e761b2330b013ff9915dd
ABDULLOH <152079357+AbdullohAbdulxamidov@users.noreply.github.com>
2024-09-29 11:11:20 +0500

    Add files via upload

---
+++ MY  PERSONAL  WEBSITE/script.js
@@ -0,0 +1,16 @@
+
+// Function to handle language change
+function changeLanguage(language) {
+    if (language === 'en') {
+        window.location.href = 'index.html'; // Redirect to the English version
+    } else if (language === 'uz') {
+        window.location.href = 'index-uz.html'; // Redirect to the Uzbek version
+    } else if (language === 'ru') {
+        window.location.href = 'index-ru.html'; // Redirect to the Russian version
+    }
+}
+
+
+
+
+// CAROUSEL ITEMS CODES
